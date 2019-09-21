29fc6d16b8087ca749fa3adf2cc8000040aafb0c
Gábor Görzsöny <gabor@gorzsony.com>
2019-09-21 22:48:23 +0200

    Added static content

---
+++ assets/js/custom.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    $('.scroll-down').click(function (event) {
+        $('html, body').animate({
+            scrollTop: $(this).offset().top + 1
+        }, 500);
+    });
+
+    $('.scroll-up').click(function (event) {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+    });
+
+    //window.sr = ScrollReveal({ reset: true });
+    sr.reveal('.reveal');
+
+    $('img').tilt({
+        glare: true,
+        maxGlare: 1,
+        scale: 1.2
+    });
+});
