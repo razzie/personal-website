1b33abfb972bf665e04656df1824456bac154acc
Gábor Görzsöny <gabor@gorzsony.com>
2020-11-24 04:58:15 +0100

    HTTP security hardening

--- cmd/razweb/main.go
+++ cmd/razweb/main.go
@@ -24,6 +24,22 @@ func main() {
 		return mainPageHandler(pr)
 	}
 
+	header := http.Header{
+		"Content-Security-Policy": {
+			"default-src 'none'",
+			"script-src 'self' 'nonce-apply-scrollreveal'",
+			"style-src 'self' 'unsafe-inline'",
+			"img-src 'self' data:",
+			"frame-ancestors 'none'",
+			"base-uri 'self'",
+			"form-action 'self'",
+		},
+		"Strict-Transport-Security": {"max-age=63072000"},
+		"X-Content-Type-Options":    {"nosniff"},
+		"X-Frame-Options":           {"DENY"},
+		"X-XSS-Protection":          {"1", "mode=block"},
+	}
+
 	srv := beepboop.NewServer()
 	srv.Layout = layout.Layout
 	srv.FaviconPNG, _ = internal.Asset("img/favicon.png")
@@ -36,6 +52,7 @@ func main() {
 		layout.CombineModules("/timeline", "Gábor Görzsöny - Project timeline", modules.Timeline()),
 		layout.CombineModules("/resume", "Gábor Görzsöny - Resume", modules.Resume()),
 	)
+	srv.Header = header
 
 	http.ListenAndServe("localhost:8080", srv)
 }
