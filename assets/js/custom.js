4ea50ab1379b04bec93e1ad6d63c396d96ce8bb5
Gábor Görzsöny <gabor@gorzsony.com>
2020-11-26 21:33:09 +0100

    Improved scrollers

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,12 +8,21 @@ if (sr.isSupported()) {
 $(document).ready(function () {
     $('.scroll-down').click(function (event) {
         var scrollTo = $(this).offset().top - 5;
-        if ((scrollTo > $(window).scrollTop() - 5) && (scrollTo < $(window).scrollTop() + 5)) {
+        if ((scrollTo > $(window).scrollTop() - 25) && (scrollTo < $(window).scrollTop() + 25)) {
             var scrollers = $('.scroll-down');
-            var target = scrollers.eq((scrollers.index($(this))+1) % scrollers.length);
-            scrollTo = target.offset().top - 5 - target.height();
+            if (scrollers.length > 1) {
+                var target = scrollers.eq((scrollers.index($(this))+1) % scrollers.length);
+                if (target.offset().top < scrollTo) {
+                    scrollTo = $('footer').offset().top
+                } else {
+                    scrollTo = target.offset().top - 5 - target.height();
+                }
+            } else {
+                scrollTo = $('footer').offset().top
+            }
         }
-        $('html, body').animate({scrollTop: scrollTo}, 500);
+        var duration = Math.sqrt(Math.abs($('html, body').scrollTop() - scrollTo)) * 16;
+        $('html, body').animate({scrollTop: scrollTo}, duration);
     });
 
     $('.scroll-up').click(function (event) {
