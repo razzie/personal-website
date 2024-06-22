fa36b9eb9202cd4ec2cb74e22bcf239ac15054ee
Gábor Görzsöny <gabor@gorzsony.com>
2024-06-22 23:48:53 +0200

    Fixed missing email after htmx boost

--- static/email.js
+++ static/email.js
@@ -1,12 +1,14 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-	const user = 'gabor';
-	const domain = 'gorzsony.com';
-	const email = user + '@' + domain;
-	const emailElement = document.getElementById('email');
+['DOMContentLoaded', 'htmx:afterSwap'].forEach(e => {
+	document.addEventListener(e, () => {
+		const user = 'gabor';
+		const domain = 'gorzsony.com';
+		const email = user + '@' + domain;
+		const emailElement = document.getElementById('email');
 
-	const mailtoLink = document.createElement('a');
-	mailtoLink.href = 'mailto:' + email;
-	mailtoLink.textContent = email;
+		const mailtoLink = document.createElement('a');
+		mailtoLink.href = 'mailto:' + email;
+		mailtoLink.textContent = email;
 
-	emailElement.appendChild(mailtoLink);
-});
+		emailElement.appendChild(mailtoLink);
+	})
+})
